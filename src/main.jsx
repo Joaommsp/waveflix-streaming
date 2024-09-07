08c07da62c0e54e60469ec195bf99a5812482f10
Joao <jmmsp2003@hotmail.com>
2024-09-07 20:00:24 -0300

    fix: change router mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
@@ -7,8 +7,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -454,9 +454,9 @@ const Home = (props) => {
               </nav>
               <div className="flex flex-col laptop:flex-row items-end laptop:items-center gap-4 laptop:gap-4 tablet:gap-2">
                 
-                  <div className="flex-col laptop:flex-row justify-end w-fit flex items-center gap-4 mb-4">
-                  <Link to={"/signin"} className="text-gray-100 font-light w-full text-end"  href="">Logout</Link>
-                  <Link to={"/login"} c className="text-gray-100 font-light w-full text-end" href="">Change Account</Link>
+                  <div className="flex-col laptop:flex-row justify-end w-full flex items-center gap-4 mb-4">
+                  <Link to={"/signin"} className="text-gray-100 font-light w-fit text-end"  href="">Logout</Link>
+                  <Link to={"/login"} c className="text-gray-100 font-light w-fit text-end" href="">Change Account</Link>
                   </div>
                 <div className="flex flex-row items-center gap-2 laptop:gap-4">
 
