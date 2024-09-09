af36d376f7ae52815435af15be448704b01134c5
Joao <jmmsp2003@hotmail.com>
2024-09-09 11:17:18 -0300

    feat: functional scroll to top component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import "./index.css";
 import App from "./App.jsx";
 
+import ScrollToTop from "./scrollToTop";
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </StrictMode>
