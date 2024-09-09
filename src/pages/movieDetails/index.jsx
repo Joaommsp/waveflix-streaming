ed66c2b0a7d704b8a7761a04099ca6013637c75e
Joao <jmmsp2003@hotmail.com>
2024-09-09 11:33:38 -0300

    feat: functional scroll to top component v02

--- src/pages/movieDetails/index.jsx
+++ src/pages/movieDetails/index.jsx
@@ -67,8 +67,14 @@ const MovieDetails = () => {
   const carrouselRef_2 = useRef(null);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+
+    const timeToScrollTop = setInterval(() => {
+      window.scrollTo(0, 0);
+
+      clearInterval(timeToScrollTop)
+    }, 500);
+
+  }, [movieID]);
 
   useEffect(() => {
     setLocalMovieID();
